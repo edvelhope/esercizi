3f6c9e6a891797318a8d1bb0e3f4f5bf940c6e1f
Edoardo <160859293+edvelhope@users.noreply.github.com>
2024-06-07 10:33:41 +0200

    Add files via upload

---
+++ Esercizi React/Styling Components - 1/Welcome.js
@@ -0,0 +1,13 @@
+import { Age } from "./Age";
+
+export function Welcome({ name = "World", age }) {
+  return (
+    <div className="welcome">
+      <p>Hello, {name}!</p>
+      {age > 18 && <Age age={age} />}
+      {age !== undefined && <Age age={age} />}
+      {age > 18 && age < 65 && <Age age={age} />}
+      {age > 18 && age < 65 && name === "John" && <Age age={age} />}
+    </div>
+  );
+}
