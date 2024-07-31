18e76ed6a564b887616474a57298d536c967d7a0
Edoardo De Rosa <160859293+edvelhope@users.noreply.github.com>
2024-07-31 15:48:40 +0200

    Add files via upload

---
+++ Esercizi Node/Add Postgres DB/server.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+import "express-async-errors"
+import morgan from 'morgan'
+import 'dotenv/config'
+import { getAll, getOneById, create, updateById, deleteById } from './controllers/planets'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.use(express.json())
+
+app.use(morgan("dev"))
+
+app.get('/api/planets', getAll)
+
+app.get('/api/planets/:id', getOneById)
+
+app.post('/api/planets', create)
+
+app.put('/api/planets/:id', updateById)
+
+app.delete("/api/planets/:id", deleteById)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
