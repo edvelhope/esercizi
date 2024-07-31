98b1ba5544c964c1d81bfd35cdf4393988ac29db
Edoardo De Rosa <160859293+edvelhope@users.noreply.github.com>
2024-07-31 18:22:38 +0200

    Add files via upload

---
+++ Esercizi Node/JWT Auth part 2/server.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import { db } from "./../db.js";
+import * as dotenv from "dotenv";
+dotenv.config();
+import jwt from "jsonwebtoken";
+
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  const user = await db.one(`SELECT * FROM users WHERE username=$1;`, username);
+  if (user && user.password === password) {
+    const payload = {
+      id: user.id,
+      username,
+    };
+    const { SECRET } = process.env;
+
+    const token = jwt.sign(payload, SECRET as string);
+    console.log(token);
+
+    await db.none(`UPDATE users SET token=$2 WHERE id=$1);`, [user.id, token]);
+    res.status(200).json({ id: user.id, username, token });
+  } else {
+    res.status(400).json({ msg: "Username or password incorrect." });
+  }
+};
+
+const signUp = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  const user = await db.oneOrNone(`SELECT * FROM users WHERE username=$1;`, username);
+  if (user) {
+    res.status(409).json({ msg: "Username already used." });
+  } else {
+    const { id } = await db.one(`INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id;`, [
+      username,
+      password,
+    ]);
+    res.status(201).json({ id, msg: "User created succesfully!" });
+  }
+};
+
+export { login, signUp };
